3a4ccdb56ebe8a449ff353331005c56c0997c5c5
ros3n <rafalrozak@gmail.com>
2015-01-06 21:20:40 +0100

    feature: compute mel filterbanks

--- GalileoSpeechRecognizer/Main.cpp
+++ GalileoSpeechRecognizer/Main.cpp
@@ -18,6 +18,9 @@ const int MICROPHONE_PIN = 0;
 const int SAMPLING_FREQ = 16000; // Hz
 const double FRAME_LENGTH = 0.025;
 const int S = 400; //number of samples ( SAMPLING_FREQ * FRAME_LENGTH ) VS has complained about not constant expression
+const int MEL_LOWER_BOUND_HZ = 300;
+const int MEL_UPPER_BOUND_HZ = 8000;
+const int FILTERBANKS = 26;
 int input[S];
 int N = firstPowerOfTwo(S);
 double *P;
@@ -91,7 +94,7 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	//pinMode(MICROPHONE_PIN, INPUT);
 
-	BYTE *samples = loadWaveFile("60Hz_7000Hz_1s.wav");
+	short *samples = loadWaveFile("60Hz_7000Hz_1s.wav");
 
 	if (samples == NULL)
 	{
@@ -103,6 +106,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	for (int i = 0; i < S; i++)
 	{
+		//Log("%d\n", (int)samples[i]);
+
 		x[i].re = (double)samples[i] * hammingWindow(i, N); //*/ 100 * sin(i * 2 * M_PI / 16) + 100;
 		x[i].im = 0;
 	}
@@ -126,18 +131,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	double m, res = 0;
 	int j;
 
-	/*for (int i = 1; i < N/2; i++)
+	for (int i = 1; i < N/2; i++)
 	{
 		m = magnitude(X[i]);
-		//Log(L"f = %d, mag = %lf\n", i * SAMPLING_FREQ / N, m);
-		Log(L"%lf\n", m);
+		Log(L"i = %d, f = %d, mag = %lf\n", i, i * SAMPLING_FREQ / N, m);
+		//Log(L"%lf\n", m);
 		if (m > res)
 		{
 			//Log(L"f = %d, mag = %lf\n", i * SAMPLING_FREQ / N, m);
 			res = m;
 			j = i;
 		}
-	}*/
+	}
 
 	Log(L"f = %d, mag = %lf\n", j * SAMPLING_FREQ / N, res);
 
@@ -145,8 +150,62 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	for (int i = 0; i < N / 2; i++)
 	{
-		P[i] = 1/N * pow(magnitude(X[i]), 2);
+		P[i] = (1.0 / (double)N) * pow(magnitude(X[i]), 2);
 	}
+
+	//Log("****\n");
+
+	double melLowerBound = 1125.0 * log(1.0 + ((double)MEL_LOWER_BOUND_HZ / 700.0));
+	double melUpperBound = 1125.0 * log(1.0 + ((double)MEL_UPPER_BOUND_HZ / 700.0));
+
+	double *fftBins = (double *)malloc(sizeof(double) * (FILTERBANKS + 2));
+
+	double diff = (melUpperBound - melLowerBound) / ((double)FILTERBANKS + 1);
+
+	Log("%lf\n", melLowerBound);
+
+	Log("%lf\n", melUpperBound);
+
+	Log("%lf\n", diff);
+
+	double filter;
+
+	for (int i = 0; i < FILTERBANKS + 2; i++)
+	{
+		filter = 700.0 * (exp((melLowerBound + (double)i * diff) / 1125.0) - 1.0);
+		fftBins[i] = floor(filter * (N + 1) / SAMPLING_FREQ);
+
+		Log("%lf - %lf\n", filter, fftBins[i]);
+	}
+
+	double **filterBanks = (double **)malloc((sizeof(double *) * FILTERBANKS));
+
+	for (int m = 1; m <= FILTERBANKS; m++)
+	{
+		filterBanks[m - 1] = (double *)malloc(sizeof(double) * N / 2);
+		for (int k = 0; k < N / 2; k++)
+		{
+			if (k >= fftBins[m - 1] && k <= fftBins[m])
+			{
+				filterBanks[m - 1][k] = (k - fftBins[m - 1]) / (fftBins[m] - fftBins[m - 1]);
+			}
+			else if (k >= fftBins[m] && k <= fftBins[m + 1])
+			{
+				filterBanks[m - 1][k] = (fftBins[m + 1] - k) / (fftBins[m + 1] - fftBins[m]);
+			}
+			else
+			{
+				filterBanks[m - 1][k] = 0;
+			}
+		}
+
+		for (int k = 0; k < N / 2; k++)
+		{
+			Log("%lf\n", filterBanks[m - 1][k]);
+		}
+		Log("\n\n\n");
+	}
+
 	//Log(L"Launching handleInput thread..\n");
 	//_beginthread(handleInput, 0, NULL);
 	//handleInput();
